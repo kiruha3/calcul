24959bc5ce29244adfa9b4188c8de5e4278069a9
kirill <kiruha3@mail.ru>
2023-10-14 17:12:18 +0700

    f

---
+++ src/main/java/com/kiruha/calcul/CalculApplication.java
@@ -0,0 +1,13 @@
+package com.kiruha.calcul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculApplication.class, args);
+	}
+
+}
